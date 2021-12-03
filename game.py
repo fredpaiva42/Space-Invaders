0967a003fe28350bf13dddf516a1689fbd2e2fb8
fredpaiva42 <fredericogdpaiva@gmail.com>
2021-12-03 16:01:17 -0300

    jogo

---
+++ game.py
@@ -0,0 +1,13 @@
+class Jogar:
+
+    def __init__(self, janela, fundo):
+        self.janela = janela
+        self.teclado = janela.get_keyboard()
+        self.fundo = fundo
+
+    def run(self):
+        self.fundo.draw()
+
+        if self.teclado.key_pressed('ESC'):
+
+            self.janela.close()
