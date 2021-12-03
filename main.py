957cf9a6c9c526d847b5d0b726f550c135b910a0
fredpaiva42 <fredericogdpaiva@gmail.com>
2021-12-03 16:00:15 -0300

    Arquivo principal

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-def print_hi(name):
-
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('Fred')
+from game import Jogar
+from menu import Menu
+from dificuldades import Dificuldade
+import dados
 
 
+menu = Menu(dados.janela, dados.fundo)
+jogo = Jogar(dados.janela, dados.fundo)
+dificuldade = Dificuldade(dados.janela, dados.fundo)
+
+while dados.GAME_STATE != 5:
+
+    if dados.GAME_STATE == 0:
+        menu.run()
+    elif dados.GAME_STATE == 1:
+        jogo.run()
+    elif dados.GAME_STATE == 3:
+        dificuldade.run()
+    elif dados.GAME_STATE == 4:
+        dados.janela.close()
+
+    dados.janela.update()
