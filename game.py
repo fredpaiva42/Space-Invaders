4e41e349bf15901a0c78204d1b34b34659a48754
fredpaiva42 <fredericogdpaiva@gmail.com>
2021-12-10 13:39:27 -0300

    adicionando o player e as funções de tiro.

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import dados
+from jogador import Jogador
 
 class Jogar:
 
@@ -6,9 +7,28 @@ class Jogar:
         self.janela = janela
         self.teclado = janela.get_keyboard()
         self.fundo = fundo
+        self.jogador = Jogador(self.janela)
+
+
+        self.tempo = 0
+        self.fps = 0
+        self.contador = 0
+        self.relogio = 0
+
 
     def run(self):
+        # FPS
+        self.relogio = self.relogio + self.janela.delta_time()
+        self.contador += 1
+
+        if self.relogio >= 1:
+            self.fps = self.contador
+            self.relogio = 0
+            self.contador = 1
+
         self.fundo.draw()
+        self.jogador.run()
+
 
         if self.teclado.key_pressed('ESC'):
             dados.GAME_STATE = 0
